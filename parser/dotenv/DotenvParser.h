ff754ba82c6d41178ef3110151813a654aa74c0c
Alejandro de Haro <a.deharo@busup.com>
2020-08-01 00:22:14 +0200

    Add ANTLR4 parser files

---
+++ parser/dotenv/DotenvParser.h
@@ -0,0 +1,162 @@
+
+// Generated from Dotenv.g4 by ANTLR 4.8
+
+#pragma once
+
+
+#include "antlr4-runtime.h"
+
+
+
+
+class  DotenvParser : public antlr4::Parser {
+public:
+  enum {
+    EQ = 1, NL = 2, WS = 3, COMMENT = 4, STRING = 5, UNQUOTED_STRING = 6
+  };
+
+  enum {
+    RuleDotenv = 0, RuleLine = 1, RulePair = 2, RuleKey = 3, RuleValue = 4
+  };
+
+  DotenvParser(antlr4::TokenStream *input);
+  ~DotenvParser();
+
+  virtual std::string getGrammarFileName() const override;
+  virtual const antlr4::atn::ATN& getATN() const override { return _atn; };
+  virtual const std::vector<std::string>& getTokenNames() const override { return _tokenNames; }; // deprecated: use vocabulary instead.
+  virtual const std::vector<std::string>& getRuleNames() const override;
+  virtual antlr4::dfa::Vocabulary& getVocabulary() const override;
+
+
+  class DotenvContext;
+  class LineContext;
+  class PairContext;
+  class KeyContext;
+  class ValueContext; 
+
+  class  DotenvContext : public antlr4::ParserRuleContext {
+  public:
+    DotenvContext(antlr4::ParserRuleContext *parent, size_t invokingState);
+    virtual size_t getRuleIndex() const override;
+    std::vector<LineContext *> line();
+    LineContext* line(size_t i);
+    antlr4::tree::TerminalNode *EOF();
+    std::vector<antlr4::tree::TerminalNode *> NL();
+    antlr4::tree::TerminalNode* NL(size_t i);
+
+    virtual void enterRule(antlr4::tree::ParseTreeListener *listener) override;
+    virtual void exitRule(antlr4::tree::ParseTreeListener *listener) override;
+   
+  };
+
+  DotenvContext* dotenv();
+
+  class  LineContext : public antlr4::ParserRuleContext {
+  public:
+    LineContext(antlr4::ParserRuleContext *parent, size_t invokingState);
+   
+    LineContext() = default;
+    void copyFrom(LineContext *context);
+    using antlr4::ParserRuleContext::copyFrom;
+
+    virtual size_t getRuleIndex() const override;
+
+   
+  };
+
+  class  EmptyLineContext : public LineContext {
+  public:
+    EmptyLineContext(LineContext *ctx);
+
+    antlr4::tree::TerminalNode *WS();
+    virtual void enterRule(antlr4::tree::ParseTreeListener *listener) override;
+    virtual void exitRule(antlr4::tree::ParseTreeListener *listener) override;
+  };
+
+  class  LineContentContext : public LineContext {
+  public:
+    LineContentContext(LineContext *ctx);
+
+    PairContext *pair();
+    std::vector<antlr4::tree::TerminalNode *> WS();
+    antlr4::tree::TerminalNode* WS(size_t i);
+    virtual void enterRule(antlr4::tree::ParseTreeListener *listener) override;
+    virtual void exitRule(antlr4::tree::ParseTreeListener *listener) override;
+  };
+
+  LineContext* line();
+
+  class  PairContext : public antlr4::ParserRuleContext {
+  public:
+    PairContext(antlr4::ParserRuleContext *parent, size_t invokingState);
+    virtual size_t getRuleIndex() const override;
+    KeyContext *key();
+    antlr4::tree::TerminalNode *EQ();
+    ValueContext *value();
+    std::vector<antlr4::tree::TerminalNode *> WS();
+    antlr4::tree::TerminalNode* WS(size_t i);
+
+    virtual void enterRule(antlr4::tree::ParseTreeListener *listener) override;
+    virtual void exitRule(antlr4::tree::ParseTreeListener *listener) override;
+   
+  };
+
+  PairContext* pair();
+
+  class  KeyContext : public antlr4::ParserRuleContext {
+  public:
+    antlr4::Token *export_token = nullptr;;
+    antlr4::Token *key_unquoted = nullptr;;
+    antlr4::Token *key_string = nullptr;;
+    KeyContext(antlr4::ParserRuleContext *parent, size_t invokingState);
+    virtual size_t getRuleIndex() const override;
+    std::vector<antlr4::tree::TerminalNode *> UNQUOTED_STRING();
+    antlr4::tree::TerminalNode* UNQUOTED_STRING(size_t i);
+    antlr4::tree::TerminalNode *WS();
+    antlr4::tree::TerminalNode *STRING();
+
+    virtual void enterRule(antlr4::tree::ParseTreeListener *listener) override;
+    virtual void exitRule(antlr4::tree::ParseTreeListener *listener) override;
+   
+  };
+
+  KeyContext* key();
+
+  class  ValueContext : public antlr4::ParserRuleContext {
+  public:
+    ValueContext(antlr4::ParserRuleContext *parent, size_t invokingState);
+    virtual size_t getRuleIndex() const override;
+    std::vector<antlr4::tree::TerminalNode *> UNQUOTED_STRING();
+    antlr4::tree::TerminalNode* UNQUOTED_STRING(size_t i);
+    std::vector<antlr4::tree::TerminalNode *> WS();
+    antlr4::tree::TerminalNode* WS(size_t i);
+    antlr4::tree::TerminalNode *STRING();
+
+    virtual void enterRule(antlr4::tree::ParseTreeListener *listener) override;
+    virtual void exitRule(antlr4::tree::ParseTreeListener *listener) override;
+   
+  };
+
+  ValueContext* value();
+
+
+private:
+  static std::vector<antlr4::dfa::DFA> _decisionToDFA;
+  static antlr4::atn::PredictionContextCache _sharedContextCache;
+  static std::vector<std::string> _ruleNames;
+  static std::vector<std::string> _tokenNames;
+
+  static std::vector<std::string> _literalNames;
+  static std::vector<std::string> _symbolicNames;
+  static antlr4::dfa::Vocabulary _vocabulary;
+  static antlr4::atn::ATN _atn;
+  static std::vector<uint16_t> _serializedATN;
+
+
+  struct Initializer {
+    Initializer();
+  };
+  static Initializer _init;
+};
+
---
+++ parser/line/LineBaseListener.cpp
@@ -0,0 +1,7 @@
+
+// Generated from Line.g4 by ANTLR 4.8
+
+
+#include "LineBaseListener.h"
+
+
