9d0ab0826e8077f961df33732fe7bb86101fa437
Alejandro de Haro <a.deharo@busup.com>
2020-08-04 13:15:26 +0200

    Update parser and file name

---
+++ src/Parser.cpp
@@ -0,0 +1,101 @@
+#include "Parser.h"
+
+#include "PairsListener.h"
+
+#include "DotenvLexer.h"
+#include "DotenvParser.h"
+#include "LineLexer.h"
+#include "LineParser.h"
+
+#include "antlr4-runtime.h"
+#include "environ.h"
+
+#include <string>
+#include <utility>
+
+using namespace dotenv;
+using namespace antlr4;
+using namespace std;
+
+
+dotenv::Parser::Parser(istream& is, const bool overwrite, const bool interpolate):
+    interpolate(interpolate),
+    overwrite(overwrite),
+    is(is)
+{
+
+}
+
+
+void dotenv::Parser::parse()
+{
+    parse_dotenv();
+    if (interpolate)
+    {
+        parse_line();
+        resolve();
+    }
+    register_env();
+}
+
+
+void dotenv::Parser::parse_dotenv()
+{
+    ANTLRInputStream input(is);
+    DotenvLexer lexer(&input);
+    CommonTokenStream tokens(&lexer);
+    tokens.fill();
+
+    DotenvParser parser(&tokens);
+    tree::ParseTree* tree = parser.dotenv();
+
+    tree::ParseTreeWalker walker;
+
+    PairsListener pairs_listener(pairs_table);
+
+    walker.walk(&pairs_listener, tree);
+}
+
+
+void dotenv::Parser::parse_line()
+{
+    for (const pair<key_type, value_type>& record: pairs_table)
+    {
+        const key_type& key = record.first;
+        const value_type& value = record.second;
+
+        ANTLRInputStream input(value);
+        LineLexer lexer(&input);
+        CommonTokenStream tokens(&lexer);
+        tokens.fill();
+
+        LineParser parser(&tokens);
+        tree::ParseTree* tree = parser.line();
+
+        tree::ParseTreeWalker walker;
+
+        // PairsListener pairs_listener(pairs_table);
+
+        // walker.walk(&pairs_listener, tree);
+    }
+}
+
+
+void dotenv::Parser::resolve()
+{
+
+}
+
+
+void dotenv::Parser::register_env() const
+{
+    const int _overwrite = overwrite ? 1 : 0;
+
+    for (const pair<key_type, value_type>& record: pairs_table)
+    {
+        const key_type& key = record.first;
+        const value_type& value = record.second;
+
+        setenv(key.c_str(), value.c_str(), _overwrite);
+    }
+}
---
+++ src/Parser.h
@@ -0,0 +1,38 @@
+#pragma once
+
+
+#include "PairsTable.h"
+
+#include <fstream>
+
+
+namespace dotenv
+{
+    class Parser
+    {
+    private:
+
+        using   key_type = std::string;
+        using value_type = std::string;
+
+    public:
+
+        Parser(std::istream& is, const bool overwrite = false, const bool interpolate = true);
+
+        void parse();
+
+    private:
+
+        void parse_dotenv();
+        void parse_line();
+        void resolve();
+        void register_env() const;
+
+        bool interpolate;
+        bool overwrite;
+        std::istream& is;
+
+        PairsTable pairs_table;
+
+    };
+}
