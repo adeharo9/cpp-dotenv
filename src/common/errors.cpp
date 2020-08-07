c731cfe19c06e6012ef77e5a2bbd194e35286e04
Alejandro de Haro <a.deharo@busup.com>
2020-08-07 19:01:58 +0200

    Fix not defined error

--- src/common/errors.cpp
+++ src/common/errors.cpp
@@ -14,3 +14,6 @@ void errors::token_error(Token* token)
     string pos = to_string(token->getCharPositionInLine());
     log.error(line + ":" + pos + " unidentified keyword \"" + token->getText() + "\"");
 }
+
+
+errors::logger log;
